55e75a867bc053172bff95eb8ad5bbdc7ac598bf
FernandoConstruiblec <fernando.nagua@construiblec.com>
2026-08-18 13:43:27 -0500

    fix(pwa): update revision number for precached index.html

--- frontend/modulo-incidentes/dev-dist/sw.js
+++ frontend/modulo-incidentes/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.miuptt5pm94"
+    "revision": "0.41q6nhcbqlc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
